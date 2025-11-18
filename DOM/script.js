7e6d3709706988a36efbb4ac95135fa483d39c76
ARAFATH-SH <alarafath.info@gmail.com>
2025-11-18 18:03:02 +0600

    done calculator html document

--- DOM/script.js
+++ DOM/script.js
@@ -58,3 +58,14 @@ function myMove(){
     }
 }
 //myMove();
+
+const p = document.getElementById("demo");
+
+const way1 = p.innerHTML;
+const way2 = p.firstChild.nodeValue;
+const way3 = p.childNodes[0].nodeValue;
+
+console.log(way1);
+console.log(way2);
+console.log(way3);
+
